9fe0cc5cd4d8f7b36db7f93d43803193cd4b5bfc
KASHIRAJA <96662570+Kashirajaji@users.noreply.github.com>
2023-05-15 08:49:41 -0300

    Update funciones de graficos.py

--- funciones de graficos.py
+++ funciones de graficos.py
@@ -14,3 +14,23 @@ def plot_roc (roc_auc):
     for i, col in enumerate(data.columns):
       plt.figure(i)
       ax = sns.countplot(x=data[col], hue=hue, data=data)
+        
+        
+
+
+def plot_stock_prices_with_labels(dates, prices, labels):
+    fig, ax = plt.subplots(figsize=(12, 6))
+    
+    ax.plot(dates, prices, '-b', label='Stock Prices')
+    
+    for date, price, label in zip(dates, prices, labels):
+        ax.annotate(label, xy=(date, price), xytext=(-20, 20),
+                    textcoords='offset points', arrowprops=dict(arrowstyle="->", color='black'))
+    
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Price')
+    ax.set_title('Stock Prices with Labels')
+    ax.legend()
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
